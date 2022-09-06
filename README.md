e8d9f697541aaf9ef8c799bbe9d5667572ed7dc8
Benziza <benizizamohamed@gmail.com>
2022-09-06 12:30:35 +0100

    Inserting elements into binary data trees

--- README.md
+++ README.md
@@ -236,4 +236,97 @@ Balanced binary data trees are a good performance choice for lookups, inserts, a
       40
 ```
 
-###
+### Inserting elements into binary data trees
+
+Elements can be inserted into binary data trees by navigating the tree levels using a queue, if we find a node that has an empty left node we create a new key as a left node for this node, otherwise if we find a node that has an empty right node we create a new key as a right node for this node.
+
+```
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BinaryTree {
+    // 1-A node in a binary tree has a key, a pointer to the left child node, and a pointer to the right child node
+    static class Node {
+        int key;
+        Node left, right;
+
+        Node(int key){
+            this.key = key;
+            left = null;
+            right = null;
+        }
+    }
+
+    static Node root;
+    static Node temp = root;
+
+    // 2-Navigate through the levels of the binary tree
+
+    static void inorder(Node temp)
+    {
+        if (temp == null)
+            return;
+
+        inorder(temp.left);
+        System.out.print(temp.key+" ");
+        inorder(temp.right);
+    }
+
+    // 3-A function to insert an element into the binary tree
+    static void insert(Node temp, int key)
+    {
+        Queue<Node> q = new LinkedList<Node>();
+        q.add(temp);
+
+        // 4-Navigate through the levels of the binary tree in search of an empty space
+        while (!q.isEmpty()) {
+            temp = q.peek();//5-get head element (the first element added)
+            q.remove();//6-remove the first element added
+
+            if (temp.left == null) {
+                temp.left = new Node(key);
+                break;
+            } else
+                q.add(temp.left);
+
+            if (temp.right == null) {
+                temp.right = new Node(key);
+                break;
+            } else
+                q.add(temp.right);
+        }
+    }
+    // 7-test the previous functions
+    public static void main(String[] args) {
+        root = new Node(10);
+        root.left = new Node(11);
+        root.left.left = new Node(7);
+        root.right = new Node(9);
+        root.right.left = new Node(15);
+        root.right.right = new Node(8);
+
+        System.out.print( "Inorder traversal before insertion:");
+        inorder(root);
+
+        /*
+			 10
+			/   \
+		   11    9
+		  / \   / \
+		 7 null 15 8  */
+
+        int key = 12;
+        insert(root, key);
+
+        /*
+			 10
+			/   \
+		   11    9
+		  / \   / \
+		 7  12 15  8  */
+        System.out.print("\nInorder traversal after insertion:");
+        inorder(root);
+    }
+}
+
+```
--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,58 +1,87 @@
-// 1-Node
-class Node
-{
-    int key;
-    Node left, right;
+import java.util.LinkedList;
+import java.util.Queue;
 
-    public Node(int item)
-    {
-        key = item;
-        left = right = null;
-    }
-}
 public class BinaryTree {
+    // 1-A node in a binary tree has a key, a pointer to the left child node, and a pointer to the right child node
+    static class Node {
+        int key;
+        Node left, right;
 
-    // 2-Binary tree root
-    Node root;
-
-    // 3-Constructors
-    BinaryTree(int key)
-    {
-        root = new Node(key);
+        Node(int key){
+            this.key = key;
+            left = null;
+            right = null;
+        }
     }
 
-    BinaryTree()
+    static Node root;
+    static Node temp = root;
+
+    // 2-Navigate through the levels of the binary tree
+
+    static void inorder(Node temp)
     {
-        root = null;
+        if (temp == null)
+            return;
+
+        inorder(temp.left);
+        System.out.print(temp.key+" ");
+        inorder(temp.right);
     }
 
+    // 3-A function to insert an element into the binary tree
+    static void insert(Node temp, int key)
+    {
+        Queue<Node> q = new LinkedList<Node>();
+        q.add(temp);
+
+        // 4-Navigate through the levels of the binary tree in search of an empty space
+        while (!q.isEmpty()) {
+            temp = q.peek();//5-get head element (the first element added)
+            q.remove();//6-remove the first element added
+
+            if (temp.left == null) {
+                temp.left = new Node(key);
+                break;
+            } else
+                q.add(temp.left);
+
+            if (temp.right == null) {
+                temp.right = new Node(key);
+                break;
+            } else
+                q.add(temp.right);
+        }
+    }
+    // 7-test the previous functions
     public static void main(String[] args) {
-        BinaryTree tree = new BinaryTree();
+        root = new Node(10);
+        root.left = new Node(11);
+        root.left.left = new Node(7);
+        root.right = new Node(9);
+        root.right.left = new Node(15);
+        root.right.right = new Node(8);
 
-        /* 4-Create root*/
-        tree.root = new Node(1);
-        /* After executing the previous statement, the tree will look like this:
-			 1
-			/ \
-		null   null
-		*/
+        System.out.print( "Inorder traversal before insertion:");
+        inorder(root);
 
-        tree.root.left = new Node(2);
-        tree.root.right = new Node(3);
-		/* 5-Node 2 and 3 become the left and right sons of Node 1
-			 1
-			/ \
-		   2   3
-		  / \ / \
-		null null null null */
+        /*
+			 10
+			/   \
+		   11    9
+		  / \   / \
+		 7 null 15 8  */
 
-        tree.root.left.left = new Node(4);
-        /* 6-Node 4 becomes the left son of Node 2
-			 1
-			/ \
-		   2   3
-		  / \ / \
-		4  null null null */
+        int key = 12;
+        insert(root, key);
 
+        /*
+			 10
+			/   \
+		   11    9
+		  / \   / \
+		 7  12 15  8  */
+        System.out.print("\nInorder traversal after insertion:");
+        inorder(root);
     }
 }
